e97153c80d25f659fa3da8b82814c8c501098342
joshualholland <joshualholland@gmail.com>
2019-03-08 14:37:21 -0600

    initial commit

---
+++ spotify_client/src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    const params = this.getHashParams();
+    this.state = {
+      loggedIn: params.access_token ? true : false
+    }
+  }
+  getHashParams() {
+    var hashParams = {};
+    var e, r = /([^&;=]+)=?([^&;]*)/g,
+        q = window.location.hash.substring(1);
+    while ( e = r.exec(q)) {
+       hashParams[e[1]] = decodeURIComponent(e[2]);
+    }
+    return hashParams;
+  }
+  render() {
+    return (
+      <div className="App">
+        <a href='http://localhost:8888'>
+          <button>Login with Spotify</button>
+        </a>
+      </div>
+    );
+  }
+}
+
+export default App;
