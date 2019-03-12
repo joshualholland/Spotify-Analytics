a9695b3ea772161a263ac5eb02607b77abf90df7
joshualholland <joshualholland@gmail.com>
2019-03-12 18:03:54 -0500

    added chart

--- spotify_client/src/components/Home.js
+++ spotify_client/src/components/Home.js
@@ -1,7 +1,38 @@
 import React, { Component } from 'react';
 import './css/Home.css';
+import CanvasJSReact from '../utils/canvasjs.react';
 import SpotifyWebApi from 'spotify-web-api-js';
+
+const CanvasJS = CanvasJSReact.CanvasJS;
+const CanvasJSChart = CanvasJSReact.CanvasJSChart;
+
 const spotifyApi = new SpotifyWebApi();
+
+const options = {
+    theme: "dark2",
+    animationEnabled: true,
+    exportFileName: "New Year Resolutions",
+    exportEnabled: true,
+    title:{
+        text: "Top Categories of New Year's Resolution"
+    },
+    data: [{
+        type: "pie",
+        showInLegend: true,
+        legendText: "{label}",
+        toolTipContent: "{label}: <strong>{y}%</strong>",
+        indexLabel: "{y}%",
+        indexLabelPlacement: "inside",
+        dataPoints: [
+            { y: 32, label: "Health" },
+            { y: 22, label: "Finance" },
+            { y: 15, label: "Education" },
+            { y: 19, label: "Career" },
+            { y: 5, label: "Family" },
+            { y: 7, label: "Real Estate" }
+        ]
+    }]
+}
 
 class Home extends Component {
     constructor() {
@@ -44,6 +75,7 @@ class Home extends Component {
             console.log(e)
         }
     }
+
     render() {
         return (
             <div className="Home">
@@ -57,6 +89,9 @@ class Home extends Component {
                             Check Now Playing
                     </button>
                     }
+                </div>
+                <div>
+                    <CanvasJSChart options={options} />
                 </div>
             </div>
 
