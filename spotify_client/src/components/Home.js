e4f1b5ab58042fa26fd2e1bffc4fa020cb324bd6
joshualholland <joshualholland@gmail.com>
2019-03-18 19:52:06 -0500

    broke apart into components

--- spotify_client/src/components/Home.js
+++ spotify_client/src/components/Home.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
 import './css/Home.css';
-import CanvasJSReact from '../utils/canvasjs.react';
 import SpotifyWebApi from 'spotify-web-api-js';
+import Genre from './Genre';
 
-const CanvasJS = CanvasJSReact.CanvasJS;
-const CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 const spotifyApi = new SpotifyWebApi();
 
@@ -28,17 +26,6 @@ class Home extends Component {
                 display_name: null,
                 profile_img: null,
                 followers: null
-            },
-            genres: {
-                rock: null,
-                rap: null,
-                soul: null,
-                folk: null,
-                indie: null,
-                classics: null,
-                country: null,
-                electronic: null,
-                other: null
             },
         }
     }
@@ -77,93 +64,12 @@ class Home extends Component {
                     followers: me.followers.total
                 }
             });
-
-            let genres = await spotifyApi.getMyTopArtists();
-            let n = 20;
-            let rockArray = [];
-            let popArray = [];
-            let rapArray = [];
-            let soulArray = [];
-            let folkArray = [];
-            let indieArray = [];
-            let classicsArray = [];
-            let countryArray = [];
-            let electronicArray = [];
-            let otherArray = [];
-            for (let i = 0; i < n; i++) {
-                genres.items[i].genres.forEach(genre => {
-                    if (genre.includes('rock') || genre.includes('metal') || genre.includes('post-grunge') || genre.includes('alternative') || genre.includes('punk')) {
-                        rockArray.push(genre)
-                    } else if (genre.includes('hip hop') || genre.includes('rap')) {
-                        rapArray.push(genre)
-                    } else if (genre.includes('pop')) {
-                        popArray.push(genre)
-                    } else if (genre.includes('soul')) {
-                        soulArray.push(genre)
-                    } else if (genre.includes('folk') || genre.includes('americana') || genre.includes('indiecoustica')) {
-                        folkArray.push(genre)
-                    } else if (genre.includes('indie') || genre.includes('singer-songwriter')) {
-                        indieArray.push(genre)
-                    } else if (genre.includes('adult standards') || genre.includes('classic')) {
-                        classicsArray.push(genre)
-                    } else if (genre.includes('country') || genre.includes('bluegrass') || genre.includes('holler')) {
-                        countryArray.push(genre)
-                    } else if (genre.includes('mellow') || genre.includes('house') || genre.includes('wave') || genre.includes('escape')) {
-                        electronicArray.push(genre)
-                    } else (
-                        otherArray.push(genre)
-                    )
-                });
-            };
-
-            this.setState({
-                genres: {
-                    rock: rockArray.length,
-                    rap: rapArray.length,
-                    soul: soulArray.length,
-                    folk: folkArray.length,
-                    indie: indieArray.length,
-                    classics: classicsArray.length,
-                    country: countryArray.length,
-                    electronic: electronicArray.length,
-                    other: otherArray.length
-                }
-            });
         } catch (e) {
             console.log(e)
         }
     }
 
     render() {
-        const options = {
-            theme: "dark2",
-            animationEnabled: true,
-            exportFileName: "Top Genres",
-            exportEnabled: true,
-            title: {
-                text: "Top Genres from your library"
-            },
-            data: [{
-                type: "pie",
-                showInLegend: true,
-                legendText: "{label}",
-                toolTipContent: "{label}: <strong>{y}%</strong>",
-                indexLabel: "{y}%",
-                indexLabelPlacement: "inside",
-                dataPoints: [
-                    { y: this.state.genres.rock, label: "Rock" },
-                    { y: this.state.genres.rap, label: "Hip Hop" },
-                    { y: this.state.genres.indie, label: "Indie" },
-                    { y: this.state.genres.folk, label: "Folk" },
-                    { y: this.state.genres.soul, label: "Soul" },
-                    { y: this.state.genres.classics, label: "Classics" },
-                    { y: this.state.genres.country, label: "Country" },
-                    { y: this.state.genres.electronic, label: "Electronic" },
-                    { y: this.state.genres.other, label: "Other" }
-                ]
-            }]
-        }
-
         return (
             <div className="Home">
                 <div className='me p-5'>
@@ -173,9 +79,7 @@ class Home extends Component {
                         <h5 className='text-white'>Followers: {this.state.me.followers}</h5>
                     </div>
                 </div>
-                <div className='genre-chart'>
-                    <CanvasJSChart options={options} />
-                </div>
+                <Genre />
                 <footer className='fixed-bottom'>
                     <div className='song-container'>
                         <img className='song-img m-3' src={this.state.nowPlaying.albumArt} />
